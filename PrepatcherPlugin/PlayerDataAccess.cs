64401d11ec865df73fe96df39e8449659b1ca91e
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2025-10-31 07:50:15 +0000

    Source generator for pd access

---
+++ PrepatcherPlugin/PlayerDataAccess.cs
@@ -0,0 +1,9 @@
+﻿namespace PrepatcherPlugin;
+
+/// <summary>
+/// Class containing members that correctly delegate to fields on <see cref="PlayerData.instance" />.
+/// 
+/// For example, PlayerDataAccess.visitedMossCave is equivalent to PlayerData.instance.visitedMossCave, except
+/// passing via the Get/Set bool events.
+/// </summary>
+public static partial class PlayerDataAccess;
