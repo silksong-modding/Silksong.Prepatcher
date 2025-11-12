f41d474b90ae4a2054aa23a3722e077926868c5e
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2025-11-12 21:51:22 +0000

    Implement access for GetBool methods

---
+++ PrepatcherPlugin/PlayerDataInternal.cs
@@ -0,0 +1,25 @@
+﻿namespace PrepatcherPlugin;
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+public static partial class PlayerDataInternal
+{
+
+    public static void IncrementInt(PlayerData pd, string intName)
+    {
+        IntAdd(pd, intName, 1);
+    }
+
+    public static void DecrementInt(PlayerData pd, string intName)
+    {
+        IntAdd(pd, intName, -1);
+    }
+
+    public static void IntAdd(PlayerData pd, string intName, int amount)
+    {
+        int current = pd.GetInt(intName); // GetIntInternal;
+        int next = current + amount;
+        pd.SetInt(intName, next);
+    }
+}
+
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
