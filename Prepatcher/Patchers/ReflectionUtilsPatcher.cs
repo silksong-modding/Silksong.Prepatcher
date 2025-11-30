233bb65292497ee86f670d5d95b4afa47d30255d
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2025-11-30 10:09:46 +0000

    More robust speediness

--- Prepatcher/Patchers/ReflectionUtilsPatcher.cs
+++ Prepatcher/Patchers/ReflectionUtilsPatcher.cs
@@ -32,17 +32,60 @@ public class ReflectionUtilsPatcher : BasePrepatcher
             return;
         }
 
+        // Patch static constructor to repair certain types
         MethodDefinition cctor = typeDef.Methods.FirstOrDefault(m =>
             m.IsStatic && m.IsConstructor && m.IsSpecialName
         );
-
         if (cctor == null)
         {
             Log.LogInfo($"Could not find static constructor");
             return;
         }
-
         InjectTypeLookup(assembly.MainModule, typeDef, cctor);
+
+        // Return null for certain non-existent types at the start of the method; putting null in the dictionary
+        // causes it to be ignored so we have to check manually
+        // This appears for example in Bone_10 when fleas are there
+        MethodDefinition getGlobalType = typeDef.Methods.FirstOrDefault(m =>
+            m.Name == "GetGlobalType"
+        );
+        if (getGlobalType == null)
+        {
+            Log.LogInfo($"Could not find GetGlobalType");
+            return;
+        }
+        PatchGetGlobalType(assembly.MainModule, typeDef, getGlobalType);
+    }
+
+    private void PatchGetGlobalType(
+        ModuleDefinition mainModule,
+        TypeDefinition typeDef,
+        MethodDefinition getGlobalType
+    )
+    {
+        ILProcessor processor = getGlobalType.Body.GetILProcessor();
+        Instruction originalFirstInstruction = getGlobalType.Body.Instructions.First();
+
+        Instruction ldarg = processor.Create(OpCodes.Ldarg_0);
+        Instruction ldstr = processor.Create(OpCodes.Ldstr, "HutongGames.PlayMaker.Actions.");
+
+        MethodReference opEqualityRef = mainModule.ImportReference(
+            typeof(string).GetMethod("op_Equality", [typeof(string), typeof(string)])
+        );
+        Instruction callOpEquality = processor.Create(OpCodes.Call, opEqualityRef);
+
+        Instruction brfalse = processor.Create(OpCodes.Brfalse, originalFirstInstruction);
+        Instruction ldnull = processor.Create(OpCodes.Ldnull);
+        Instruction ret = processor.Create(OpCodes.Ret);
+
+        processor.InsertBefore(originalFirstInstruction, ldarg); // Load argument
+        processor.InsertBefore(originalFirstInstruction, ldstr); // Load corrupted string
+        processor.InsertBefore(originalFirstInstruction, callOpEquality); // check if they are equal
+        processor.InsertBefore(originalFirstInstruction, brfalse); // Skip to original first instruction if they are not equal
+        processor.InsertBefore(originalFirstInstruction, ldnull); // Load null
+        processor.InsertBefore(originalFirstInstruction, ret); // return null
+
+        getGlobalType.Body.OptimizeMacros();
     }
 
     private void InjectTypeLookup(
